a4a7db46d2a6a63597d45256bde54193eefb5c64
Ville <ville.virnala@gmail.comm>
2020-11-12 19:31:09 +0200

    appjs lisätty

---
+++ app.js
@@ -0,0 +1,7 @@
+// LUO FUNKTIO TERVEHDI, JOKA OTTAA PARAMETRINA NIMEN JA TERVEHDYKSEN
+function tervehdi( nimi ) {
+    console.log( "Hei " + nimi );
+}
+let nimi = "Ville";
+// KUTSUTAAN FUNKTIOTA
+tervehdi( nimi );
