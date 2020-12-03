f24588701dd6fc0209c7e6c5db11d67f8650d774
Ville <ville.virnala@gmail.comm>
2020-12-03 15:22:40 +0200

    server tiedosto lisätty

---
+++ server.js
@@ -0,0 +1,11 @@
+
+
+// Luodaan yhteys Node.js:n ja MongoDB:n välille mongoose-paketin asentamisella.
+
+// Otetaan Mongoose käyttöön.
+
+// Yhdistetään tietokanta connect()-komennolla ja MongoDB Atlaksesta saatavalla connection stringillä ja useUnifiedTopology: true, useNewUrlParser: true asetuksilla.
+
+// Haetaan tietokannasta treenejä.
+
+// Näytetään käyttäjän hakema treeni sivustolla.
