f185e71c2e13edda1c3b880a0dec17342230b020
Ville <ville.virnala@gmail.comm>
2020-12-17 20:32:06 +0200

    Form validation lisätty

--- public/update.js
+++ public/update.js
@@ -7,3 +7,26 @@ document.body.onload = function() {
 function getParam(param) {
     return new URLSearchParams(window.location.search).get(param);
 }
+
+document.forms[0].onsubmit = function() {
+    
+    
+    let inputs = document.getElementsByTagName("input");
+    let formOk = true;
+    
+    for(let i = 0; i < inputs.length; i++) {
+        if(inputs[i].type != "submit" ) {
+    if(inputs[i].value == "") {
+        inputs[i].style = "border: 1px solid red;";
+        formOk = false;
+    }else {
+        inputs[i].style = "border: 1px solid black;"
+          }
+      }
+    }
+    if(formOk) {
+       
+    }else {
+     return formOk;
+    }
+}
